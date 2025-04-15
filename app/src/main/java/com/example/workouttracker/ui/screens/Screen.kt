c58f609a410acc33f8c80103a01e8a30ce4ddace
Yusuf Taral <yutaral99@gmail.com>
2025-04-15 18:41:25 +0300

    Add separate functions for loading dialog, snackbar and vibration

--- app/src/main/java/com/example/workouttracker/ui/screens/Screen.kt
+++ app/src/main/java/com/example/workouttracker/ui/screens/Screen.kt
@@ -1,5 +1,6 @@
 package com.example.workouttracker.ui.screens
 
+import android.content.Context
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -30,7 +31,6 @@ import com.example.workouttracker.viewmodel.MainViewModel
 @Composable
 fun Screen(vm: MainViewModel) {
     val snackbarHostState = remember { SnackbarHostState() }
-    var showLoading by rememberSaveable { mutableStateOf(false) }
     val context = LocalContext.current
 
     Scaffold(
@@ -38,42 +38,58 @@ fun Screen(vm: MainViewModel) {
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
     ) { innerPadding ->
         WorkoutTrackerTheme {
-
-            // Show loading
-            LaunchedEffect(LoadingManager.events) {
-                LoadingManager.events.collect { isLoading ->
-                    showLoading = isLoading
-                }
-            }
-
-            if (showLoading) {
-                RequestInProgressSpinner()
-            }
-
-            // Show snackbar
-            LaunchedEffect(SnackbarManager.events) {
-                SnackbarManager.events.collect { event ->
-                    snackbarHostState.currentSnackbarData?.dismiss()
-
-                    val message = if (event.messageId > 0) getString(context, event.messageId)
-                    else event.message
-
-                    snackbarHostState.showSnackbar(
-                        message = message,
-                        actionLabel = null,
-                        withDismissAction = true
-                    )
-                }
-            }
-
-            // Trigger vibrations
-            LaunchedEffect(VibrationManager.events) {
-                VibrationManager.events.collect { event ->
-                    VibrationManager.makeVibration(context, event)
-                }
-            }
+            ShowLoading()
+            ShowSnackbar(snackbarHostState, context)
+            MakeVibration(context)
 
             Navigation(modifier = Modifier.padding(innerPadding), vm = vm)
+        }
+    }
+}
+
+/** Composable to show/hide the loading dialog */
+@Composable
+private fun ShowLoading() {
+    var showLoading by rememberSaveable { mutableStateOf(false) }
+
+    // Show loading
+    LaunchedEffect(Unit) {
+        LoadingManager.events.collect { isLoading ->
+            showLoading = isLoading
+        }
+    }
+
+    if (showLoading) {
+        RequestInProgressSpinner()
+    }
+}
+
+/** Composable function to show snackbar */
+@Composable
+private fun ShowSnackbar(snackbarHostState: SnackbarHostState, context: Context) {
+    LaunchedEffect(Unit) {
+        SnackbarManager.events.collect { event ->
+            snackbarHostState.currentSnackbarData?.dismiss()
+
+            val message = if (event.messageId > 0) getString(context, event.messageId)
+            else event.message
+
+            snackbarHostState.showSnackbar(
+                message = message,
+                actionLabel = null,
+                withDismissAction = true
+            )
+        }
+    }
+}
+
+/** Composable function to trigger vibration effect */
+@Composable
+private fun MakeVibration(context: Context) {
+    // Trigger vibrations
+    LaunchedEffect(Unit) {
+        VibrationManager.events.collect { event ->
+            VibrationManager.makeVibration(context, event)
         }
     }
 }
