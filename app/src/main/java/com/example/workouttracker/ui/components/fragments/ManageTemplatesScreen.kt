b64b06123c7294f08773e0d5b65b67a4d3f257e0
Yusuf Taral <yutaral99@gmail.com>
2025-05-24 17:31:47 +0300

    Remove buttons from manage templates screen

--- app/src/main/java/com/example/workouttracker/ui/components/fragments/ManageTemplatesScreen.kt
+++ app/src/main/java/com/example/workouttracker/ui/components/fragments/ManageTemplatesScreen.kt
@@ -8,25 +8,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.workouttracker.R
-import com.example.workouttracker.ui.components.reusable.ImageButton
 import com.example.workouttracker.ui.components.reusable.InputField
 import com.example.workouttracker.ui.components.reusable.Spinner
 import com.example.workouttracker.ui.components.reusable.WorkoutItem
 import com.example.workouttracker.ui.theme.LazyListBottomPadding
-import com.example.workouttracker.ui.theme.PaddingSmall
 import com.example.workouttracker.ui.theme.PaddingVerySmall
 import com.example.workouttracker.ui.theme.WorkoutTrackerTheme
 import com.example.workouttracker.viewmodel.ManageTemplatesViewModel
@@ -80,22 +74,6 @@ fun ManageTemplatesScreen(vm: ManageTemplatesViewModel = hiltViewModel()) {
                 }
             }
         }
-
-        ImageButton(
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .padding(start = PaddingSmall),
-            onClick = {},
-            image = Icons.AutoMirrored.Filled.ArrowBack
-        )
-
-        ImageButton(
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(end = PaddingSmall),
-            onClick = {},
-            image = Icons.Default.Add
-        )
     }
 }
 
